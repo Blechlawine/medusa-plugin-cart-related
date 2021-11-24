41511b64e18a1f748980c4c085fcd715d7d0a5c4
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-11-24 20:26:00 +0100

    fix cors issue

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,20 @@
 import express from "express";
+import cors from "cors";
 import { Not, In } from "typeorm";
 import { getConfigFile } from "medusa-core-utils";
 
 const { Router } = express;
 
 export default (rootDirectory) => {
-    const config = getConfigFile(rootDirectory, "medusa-config").configModule.projectConfig || {
-        relatedAmount: 3,
-    };
+    const config = getConfigFile(rootDirectory, "medusa-config").configModule.projectConfig;
     const router = Router();
+
+    const corsOptions = {
+        origin: config.store_cors.split(","),
+        credentials: true,
+    };
+
+    app.options("/store/carts/:id/related", cors(corsOptions)); 
 
     router.get("/store/carts/:id/related", async (req, res) => {
         const cartService = req.scope.resolve("cartService");
@@ -46,7 +52,7 @@ export default (rootDirectory) => {
                         "collection",
                         "type",
                     ],
-                    take: config.relatedAmount,
+                    take: config.relatedAmount || 3,
                 }
             );
             res.status(200).json(result);
